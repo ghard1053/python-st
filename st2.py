0d134a5c28c6a9ff39193c228d8c991d014a4f64
Takao <sukisya70@gmail.com>
2019-03-26 23:58:54 +0900

    190326

--- st2.py
+++ st2.py
@@ -7,3 +7,19 @@ r = random.randint(1, 6)
 datetime.date.today()
 t = datetime.date.now()
 t.strftime("%Y/%m/%d")
+
+
+a_file = open("mt7_7.txt", encoding="utf-8")
+s = a_file.read()
+a_file.close()
+
+a_file = open("test.txt", mode="w", encoding="utf-8")
+a_file.write("hello\n")
+a_file.write("world\n")
+a_file.close()
+
+a_file = open("test.txt", mode="w", encoding="utf-8")
+try:
+    a_file.write()
+finally:
+    a_file.close()
