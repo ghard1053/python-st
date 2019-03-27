567055498c9f1e88382c74861b37a803d1c99240
Takao <sukisya70@gmail.com>
2019-03-27 23:56:51 +0900

    190327

--- st2.py
+++ st2.py
@@ -23,3 +23,18 @@ try:
     a_file.write()
 finally:
     a_file.close()
+
+
+with open("test.txt", mode="w") as f:
+    f.write("hello\n")
+    f.write("world\n")
+
+with open("mt7_7.txt", encoding="utf-8") as tf:
+    for line in tf:
+        print(line)
+
+key = "find"
+with open("mt7_7.txt", encoding="utf-8") as tf:
+    for i, line in enumerate(tf):
+        if line.find(key) >= 0:
+            print(i+1, ":", line)
