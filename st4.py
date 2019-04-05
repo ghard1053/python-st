6937658242fbb2630dce29e5e625d59cf448a424
Takao <sukisya70@gmail.com>
2019-04-05 23:46:00 +0900

    190405

--- st4.py
+++ st4.py
@@ -22,3 +22,73 @@ class BMI:
         self.weight = weight
         self.height = height
         self.calcBMI()
+    
+
+# --------
+class Car:
+    # ****
+
+class Van(Car):
+    def __init__(self, owner):
+        super().__init__(owner)
+        # ****
+
+import car
+
+car1 = car.Van("john")
+
+
+# --------
+class Game:
+    def __goal(self):
+        print("")
+    
+    def play(self):
+        print("")
+
+game = Game()
+game.play()
+game.__goal() # error
+
+# --------
+class Empty: pass
+
+o = Empty()
+
+
+# --------
+class Hoge:
+    @staticmethod
+    def introduce():
+        print("Hoge")
+
+Hoge.introduce()
+
+
+# --------
+
+import json
+import urllib.request
+
+class Kawase:
+    API = ""
+
+    def __get_api(self):
+        res = urllib.request.urlopen(Kawase.API)
+        return res.read().decode('utf8')
+
+    def __analize_result(self, json_str):
+        return json.loads(json_str)
+
+    def get_result(self):
+        json_str = self.__get_api()
+        return self.__analize_result(json_str)
+
+    @staticmethod
+    def get_usd_jpy():
+        kawase = Kawase()
+        data = kawase.get_result()
+        usd = data.get("JPY", -1)
+        return usd
+
+print("USD:JPY = 1:", Kawase.get_usd_jpy())
