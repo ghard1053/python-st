baa08d764420708c6fd0a9fd6a62f8b0c2fc9a75
Takao <sukisya70@gmail.com>
2019-04-15 23:58:37 +0900

    190415

---
+++ py2/st3.py
@@ -0,0 +1,16 @@
+
+# グラフ描画
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7]
+y = [61, 62, 63, 62, 67, 58, 54]
+
+plt.plot(x, y)
+plt.grid(color='0.8')
+plt.slow()x
+
+# y = 3x
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
