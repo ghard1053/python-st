fff7db5d1ba669c4e67b69c723704eca1e967960
Takao <sukisya70@gmail.com>
2019-04-16 23:46:04 +0900

    190416

--- py2/st3.py
+++ py2/st3.py
@@ -49,3 +49,45 @@ plt.plot(x, y2)
 plt.axis('equal')
 plt.grid(color='0.8')
 plt.show()
+
+
+# 垂直二等分線
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+
+a1 = (5-1)/(6-0)
+b1 = 1
+
+cx = (0 + 6) / 2
+cy = (1 + 5) / 2
+
+a2 = -1 / a1
+b2 = cy - a2*cx
+
+x = np.arange(0, 7)
+y1 = a1*x + b1
+y2 = a2*x + b2
+
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
+
+
+# 三角比を使用して円を描画
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+
+th = np.arange(0, 360)
+
+r = 10
+x = r * np.cos(np.radians(th))
+y = r * np.sin(np.radians(th))
+
+plt.plot(x, y)
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
