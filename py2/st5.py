8675bd43e9cc4bbb41632cf30e6a733f120846e1
Takao <sukisya70@gmail.com>
2019-04-22 23:58:54 +0900

    190422

--- py2/st5.py
+++ py2/st5.py
@@ -20,3 +20,29 @@ A = np.matrix([[5, 3], [2, 1]])
 inv_A = np.linalg.inv(A)
 B = np.matrix([[9], [4]])
 inv_A * B
+
+# ベクトルと行列
+import numpy as np
+p = np.matrix([[3], [2]])
+A = np.matrix([[2, 0], [1, 2]])
+A * p
+
+# x軸に対して線対称
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+
+p = np.matrix([[1, 3, 3, 1], [1, 1, 2, 1]])
+
+A = np.matrix([[1, 0], [0, -1]])
+
+p2 = A * p
+print(p2)
+
+p = np.array(p)
+p2 = np.array(p2)
+plt.plot(p[0, :], p[1, :])
+plt.plot(p2[0, :], p2[1, :])
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
