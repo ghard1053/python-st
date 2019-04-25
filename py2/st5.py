1bb5fefd73373315a99febb7be99a358a8a64133
Takao <sukisya70@gmail.com>
2019-04-25 23:58:07 +0900

    190425

--- py2/st5.py
+++ py2/st5.py
@@ -68,3 +68,14 @@ p = np.matrix([[3, 3, 5, 5, 3], [3, 1, 1, 3, 3]])
 
 th = np.radians(45)
 A = np.matrix([[np.cos(th), np.sin(-th)], [np.sin(th), np.cons(th)]])
+
+p2 = A * p
+print(p2)
+
+p = np.array(p)
+p2 = np.array(p2)
+plt.plot(p[0, :], p[1, :])
+plt.plot(p2[0, :], p2[1, :])
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
