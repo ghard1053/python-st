a709ebcce0571a133f575f1165c395a89474f147
Takao <sukisya70@gmail.com>
2019-04-26 22:21:36 +0900

    190426

--- py2/st5.py
+++ py2/st5.py
@@ -79,3 +79,19 @@ plt.plot(p2[0, :], p2[1, :])
 plt.axis('equal')
 plt.grid(color='0.8')
 plt.show()
+
+# 平行移動
+p = np.matrix([[1, 3, 3, 1], [1, 1, 2, 1], [1, 1, 1, 1]])
+
+A = np.matrix([[1, 0, 2], [0, 1, 3], [0, 0, 1]])
+
+p2 = A * p
+print(p2)
+
+p = np.array(p)
+p2 = np.array(p2)
+plt.plot(p[0, :], p[1, :])
+plt.plot(p2[0, :], p2[1, :])
+plt.axis('equal')
+plt.grid(color='0.8')
+plt.show()
